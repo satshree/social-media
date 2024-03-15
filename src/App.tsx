9ff931fb11a159d3f19c2d8429fab821a84c201e
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-15 08:53:47 -0500

    home page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
+
 import routes from "./routes";
 
 function App() {
   return (
     <div className="page">
+      <Header />
       <Routes>
         {routes.map((route) => (
           <Route key={route.text} path={route.to} element={route.element} />
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,67 @@
-import { Link } from "react-router-dom";
+// import PostForm from "../../components/PostForm";
+import Post from "../../components/Post";
 
-import { getRoute } from "../../routes";
+import { Post as PostData } from "../../types/data";
+
+import create from "../../assets/create.svg";
+import style from "./home.module.css";
 
 function Home() {
+  const posts: PostData[] = [
+    {
+      id: 123,
+      title: "Test",
+      content:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      updated: new Date(),
+    },
+    {
+      id: 1212,
+      title: "Test",
+      content:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      updated: new Date(),
+    },
+    {
+      id: 4657,
+      title: "Test",
+      content:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      updated: new Date(),
+    },
+    {
+      id: 23,
+      title: "Test",
+      content:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      updated: new Date(),
+    },
+  ];
+
   return (
-    <div>
-      Home
-      <br />
-      <Link to={getRoute("components").to}>Go to components</Link>
+    <div className={style["home-page"]}>
+      {posts.length === 0 ? (
+        <div className={style.empty}>
+          <img
+            className={style["create-img"]}
+            src={create}
+            alt="create new post"
+          />
+          <br />
+          <br />
+          Add a new post
+        </div>
+      ) : (
+        <>
+          {posts.map((post) => (
+            <Post post={post} />
+          ))}
+          <br />
+          <div className="center">
+            <small>End of thread</small>
+          </div>
+        </>
+      )}
     </div>
   );
 }
