88950ff5132c8124fcb7c83615cae86549c8a60a
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-15 09:12:19 -0500

    add modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,29 @@
+import { ModalProps } from "./types";
+
+import style from "./modal.module.css";
+import { useEffect, useState } from "react";
+
+function Modal(props: ModalProps) {
+  const [display, setDisplay] = useState("none");
+
+  useEffect(() => {
+    if (props.isOpen) {
+      setDisplay("block");
+    } else {
+      setDisplay("none");
+    }
+  }, [props.isOpen]);
+
+  return (
+    <div className={style.modal} style={{ display }}>
+      <div className={style["modal-content"]}>
+        <span className="close" onClick={props.onClose}>
+          &times;
+        </span>
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
